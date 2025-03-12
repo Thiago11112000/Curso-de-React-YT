67f997e6449ca92a5b664718bf6e2a95f952178c
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2025-03-12 13:04:02 -0300

    Criando componentes em React

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
 
@@ -15,6 +16,8 @@ function App() {
       <p> Olá, {newName}</p>
       <p> Soma {sum(1,2)}</p>
        <img src={url} alt="Minha imagem" />
+
+       < HelloWorld />
     </div>
   );
 }
