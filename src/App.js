ada9371aea88d1b7af4757f6351c10cff4966efa
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2025-03-12 16:11:28 -0300

    Fragments

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Frase from './components/Frase';
+import List from './components/List';
 
 function App() {
 
@@ -21,6 +22,7 @@ function App() {
      profissao="programador"
      foto="https://placehold.co/150"
      />
+     <List />
     </div>
   );
 }
